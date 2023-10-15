71b0b7d0a0cf3d8f18da6d8b7af710bc128946a7
TaturinArtem <144136367+TaturinArtem@users.noreply.github.com>
2023-10-15 15:27:59 +0500

    Task0
    Task0

---
+++ Tyuiu.TaturinAM.Sprint2.Task0.V9.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.TaturinAM.Sprint2.Task0.V9.Lib
+{
+    public class DataService : ISprint2Task0V9
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] array = new bool[6];
+
+            array[0] = x == y + 679;
+            array[1] = x != y + 1000;
+            array[2] = x < y + 1000;
+            array[3] = x > y ;
+            array[4] = x <= y + 1000;
+            array[5] = x >= y +1000;
+
+            return array;
+        }
+    }
+}
---
+++ Tyuiu.TaturinAM.Sprint2.Task0.V9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.TaturinAM.Sprint2.Task0.V9.Lib;
+namespace Tyuiu.TaturinAM.Sprint2.Task0.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 1054; int y = 375;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            Console.Title = "Спринт #2 | Выполнил: Татурин А. М. | СМАРТб-23-1";
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("- Спринт #2                                                               -");
+            Console.WriteLine("- Тема: Операции сравнения                                                -");
+            Console.WriteLine("- Задание #0                                                              -");
+            Console.WriteLine("- Вариант #9                                                              -");
+            Console.WriteLine("- Выполнил: Татурин Артем Максимович | СМАРТб-23-1                        -");
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("- Условие: Написать программу из операций сравнений                       -");
+            Console.WriteLine("- (==, !=, <, >, <=, >=, последовательность операций не должна нарушаться)-");
+            Console.WriteLine("- и арифметических выражений, которая вернет логическую                   -");
+            Console.WriteLine("- последовательность(массив): (True, True, True, True, True, False)       -");
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("- Исходные данные:                                                        -");
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("- x = 1054, y = 375                                                       -");
+            Console.WriteLine("---------------------------------------------------------------------------");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
