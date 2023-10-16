f5099d81a0c7c13713590fb8619556a787befb4e
TaturinArtem <144136367+TaturinArtem@users.noreply.github.com>
2023-10-16 14:05:52 +0500

    Task2
    Task2

---
+++ Tyuiu.TaturinAM.Sprint2.Task2.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TaturinAM.Sprint2.Task2.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TaturinAM.Sprint2.Task2.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("790a427c-3361-4260-b848-245c33686fcc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.TaturinAM.Sprint2.Task2.V14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.TaturinAM.Sprint2.Task2.V14.Lib;
+namespace Tyuiu.TaturinAM.Sprint2.Task2.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Татурин А. М. | СМАРТб-23-1";
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("- Спринт #2                                                               -");
+            Console.WriteLine("- Тема: Оператор if – полная и короткая форма записи                      -");
+            Console.WriteLine("- Задание #2                                                              -");
+            Console.WriteLine("- Вариант #14                                                             -");
+            Console.WriteLine("- Выполнил: Татурин Артем Максимович | СМАРТб-23-1                        -");
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("- Условие: Написать программу на, которая запрашивает целые значения с    -");
+            Console.WriteLine("- клавиатуры и вычисляет находится ли точка с координатами X,Y в          -");
+            Console.WriteLine("- заштрихованной области.                                                 -");
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("Введите значение Х:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Результат: " + ds.CheckDotInShadedArea(x, y));
+            Console.ReadKey();
+        }
+    }
+}
